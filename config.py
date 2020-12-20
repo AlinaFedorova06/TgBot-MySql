6df7bf6dc6a9bf22f185de39b23348411955494c
AlinaFedorova06 <74902255+AlinaFedorova06@users.noreply.github.com>
2020-12-20 20:17:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#API Token
+API_TOKEN = '1403015492:AAF5RnUmzD_Uy9BRpmRYpe2TkjlgjqXquDs'
+
+#Admin ID
+admin_id = 624513451
+
+#Database connection settings
+dbHost = 'localhost'
+dbUser = 'root'
+dbPassword = '***********'
+dbName = 'tBot'
+
